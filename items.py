07f8af8b60f0951c6d77319bfd1d0266a932207f
DasLampe <daslampe@lano-crew.org>
2024-04-04 22:34:41 +0200

    bump to newest minio version
    Including a bunch of changes for administration and just maintenance modifications.

--- items.py
+++ items.py
@@ -1,71 +1,60 @@
-minio = node.metadata.get('minio', {})
-
-actions = {}
-
-default_path = '/usr/local/bin/minio'
-default_url = 'https://dl.min.io/server/minio/release/linux-amd64/minio.{}'.format(
-    minio.get('version', 'RELEASE.2021-01-05T05-22-38Z')
-)
-default_data_path = '/media/cache'
+cfg = node.metadata.get('minio', {})
 
 downloads = {
-    minio.get('path', default_path): {
-        'url': minio.get('download_url', default_url),
-        'sha256': minio.get('checksum', 'c18280dad1703f3f0a9041d40b34399836c4d2f94b05cf30adc75898495bd80e'),
-        'owner': minio.get('user'),
-        'group': minio.get('group'),
-        'mode': '0750',
+    f'/tmp/minio_{cfg.get("version")}.deb': {
+        'url':  f'https://dl.min.io/server/minio/release/linux-amd64/minio_{cfg.get("version")}_amd64.deb',
+        'sha256': cfg.get('checksum'),
+        'owner': cfg.get('user'),
+        'group': cfg.get('group'),
         'tags': [
             'minio_install'
         ],
+    }
+}
+
+actions = {
+    'install_minio': {
+        'command': f'dpkg -i /tmp/minio_{cfg.get("version")}.deb',
+        'needs': [
+            f'download:/tmp/minio_{cfg.get("version")}.deb',
+        ],
+        'tags': [
+            'minio_install',
+        ],
         'triggers': [
             'svc_systemd:minio.service:restart',
-        ]
+        ],
+    },
+    'daemon_reload': {
+        'command': 'systemctl daemon-reload',
+        'triggered': True,
+        'triggers': [
+            'svc_systemd:minio.service:restart',
+        ],
     }
 }
 
 files = {
-    '/home/minio/environment': {
-        'source': 'home/minio/environment',
-        'content_type': 'jinja2',
-        'context': {
-            'data_path': minio.get('data_path', default_data_path),
-            'access_key': minio.get('access_key', repo.vault.password_for(
-                'minio_cache_accesskey_{}'.format(node.hostname))),
-            'secret_key': minio.get('secret_key', repo.vault.password_for(
-                'minio_cache_secretkey_{}'.format(node.hostname))),
-        },
-        'owner': minio.get('user'),
-        'group': minio.get('group'),
-        'tags': [
-            'minio_install'
-        ],
-        'triggers': [
-            'svc_systemd:minio.service:restart',
-        ]
-    },
     '/etc/systemd/system/minio.service': {
-        'source': 'etc/systemd/system/minio.service',
+        'source': 'etc/systemd/system/minio.service.j2',
         'content_type': 'jinja2',
         'context': {
-            'user': minio.get('user'),
-            'group': minio.get('group'),
-            'path': minio.get('path', default_path),
-            'environment_file': '/home/minio/environment',
+            'cfg': cfg,
         },
         'tags': [
             'minio_install'
         ],
         'triggers': [
             'svc_systemd:minio.service:restart',
-        ]
+            'action:daemon_reload',
+        ],
     }
 }
 
 directories = {
-    minio.get('data_path', default_data_path): {
-        'owner': minio.get('user'),
-        'group': minio.get('group'),
+    cfg.get('data_path'): {
+        'owner': cfg.get('user'),
+        'group': cfg.get('group'),
         'tags': [
             'minio_install'
         ]
--- metadata.py
+++ metadata.py
@@ -1,9 +1,36 @@
 defaults = {
     'minio': {
+        'version': '20240330094156.0.0',
+        'checksum': 'ec5ba2c63b9815ea8e68a809240538b889fdbe22a8164dfe525c79b8986421b1',
+        'path': '/usr/local/bin/minio',
         'user': 'minio',
         'group': 'minio',
-    }
+        'address': '',
+        'port': '9000',
+        'console_port': '9001',
+        'server_url': 'https://minio.example.org',
+        #'console_url': 'https://minio.example.org/minio/ui',
+        'accept_iptables': False,
+        'data_path': '/media/cache',
+
+        'root_user': 'root',
+        'root_password': repo.vault.password_for(f'minio_root_user_{node.hostname}').value,
+    },
 }
+
+@metadata_reactor
+def compatibility_minio(metadata):
+    return_value = {
+        'minio': {},
+    }
+
+    # Rename access_key and secret_key to root_user and root_password
+    if metadata.get('minio/access_key', '') and not metadata.get('minio/root_user'):
+        return_value['minio']['root_user'] = metadata.get('minio/access_key')
+    if metadata.get('minio/secret_key', '') and not metadata.get('minio/root_password'):
+        return_value['minio']['root_password'] = metadata.get('minio/secret_key')
+
+    return return_value
 
 @metadata_reactor
 def add_minio_user(metadata):
@@ -20,7 +47,7 @@ def add_minio_user(metadata):
 
 @metadata_reactor
 def add_minio_iptables(metadata):
-    if not node.has_bundle('iptables'):
+    if not node.has_bundle('iptables') or not metadata.get('minio/accept_iptables'):
         raise DoNotRunAgain
 
     interfaces = ['main_interface']
@@ -30,6 +57,10 @@ def add_minio_iptables(metadata):
         iptables_rules += repo.libs.iptables.accept(). \
             input(interface). \
             tcp(). \
-            dest_port(9000)
+            dest_port(metadata.get('minio/port'))
+        iptables_rules += repo.libs.iptables.accept(). \
+            input(interface). \
+            tcp(). \
+            dest_port(metadata.get('minio/console_port'))
 
     return iptables_rules
